c608aa256a3fc4c186e119b7a637d989f1fb2de0
Bert Peters <bert@bertptrs.nl>
2019-01-20 01:17:40 +0100

    Implement argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
+#[macro_use]
+extern crate clap;
+
+use std::process::exit;
+
+use clap::Arg;
+
 mod networks;
 
 /// Default install location of network files.
 const DEFAULT_INSTALL_PATH: &str = "/var/lib/iwd";
 
 fn main() {
-    println!("Hello, world!");
+    let matches = app_from_crate!()
+        .arg(Arg::with_name("input")
+            .conflicts_with("dir")
+            .help("Profile files to process")
+            .multiple(true))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output-dir")
+            .default_value(DEFAULT_INSTALL_PATH))
+        .arg(Arg::with_name("dir")
+            .help("Directory of profiles to process")
+            .long("input-dir")
+            .short("dir")
+            .conflicts_with("input")
+            .takes_value(true))
+        .get_matches();
+
+    if let Some(dir) = matches.value_of("dir") {
+        println!("Reading profiles from {}", dir);
+    } else if let Some(files) = matches.values_of("input") {
+        for file in files {
+            println!("Reading profile {}", file);
+        }
+    } else {
+        eprintln!("Need to specify input files or input dir.");
+        exit(1);
+    }
 }
